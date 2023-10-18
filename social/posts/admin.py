6693d8ccafbcf93c501bc0414e31e84be4910306
hudaifa <hudaifa890@gmail.com>
2023-10-18 11:11:03 +0300

    add post

--- social/posts/admin.py
+++ social/posts/admin.py
@@ -1,37 +1,23 @@
 from django.contrib import admin
 
-from .models import Gallery, Post
-
-
-class GalleryAdmin(admin.ModelAdmin):
-    list_display = [
-        "post",
-        "is_active",
-        "timestamp",
-    ]
+from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = [
-        "truncated_content",
+        "short_content",
         "user",
         "timestamp",
     ]
 
-    def truncated_content(self, obj):
-        """
-        1. Split the content into words
-        2. Take the first three words and join them back into string
-        3. If there are more than three words, add an ellipsis (...) to indicate truncation
-        """
+    def short_content(self, obj):
         words = obj.content.split()
         short_content = " ".join(words[:5])
         if len(words) > 5:
             short_content += " ..."
         return short_content
 
-    truncated_content.short_description = "Truncated Content"
+    short_content.short_description = "Short Content"
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Gallery, GalleryAdmin)
--- social/posts/models.py
+++ social/posts/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -9,7 +7,7 @@ class Post(models.Model):
         EVERYONE = "EVN", "Everyone"
 
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    content = models.CharField(max_length=500, blank=True, null=True)
+    content = models.TextField(max_length=500, blank=True, null=True)
     slug = models.SlugField(max_length=600, blank=True, null=True)
     image = models.FileField(upload_to="post", blank=True, null=True)
     likes = models.ManyToManyField("users.User", related_name="likes", blank=True)
@@ -18,24 +16,12 @@ class Post(models.Model):
     visibility = models.CharField(max_length=10, choices=Visibility.choices, default=Visibility.EVERYONE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        words = self.content.split()
-        short_content = " ".join(words[:5])
-        if len(words) > 5:
-            short_content += " ..."
-        return short_content
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = f"{slugify(self.content)}"
-        super().save(*args, **kwargs)
-
-
-class Gallery(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="gallery", blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    timestamp = models.DateTimeField(timezone.now())
+    # def __str__(self):
+    #     words = self.content.split()
+    #     short_content = " ".join(words[:5])
+    #     if len(words) > 5:
+    #         short_content += " ..."
+    #     return short_content
 
     def __str__(self):
-        return str(self.post)
+        return str(self.content)[:5]
