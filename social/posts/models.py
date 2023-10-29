d1efbd405e76f054a3cd93b30a27f9603966bef0
hudaifa <hudaifa890@gmail.com>
2023-10-29 21:58:32 +0300

    new fields to the post model

--- social/posts/models.py
+++ social/posts/models.py
@@ -13,6 +13,13 @@ class Post(models.Model):
     slug = models.SlugField(max_length=600, blank=True, null=True)
     image = models.FileField(upload_to=post_photo_path, blank=True, null=True)
     likes = models.ManyToManyField("users.User", related_name="likes", blank=True)
+    reposted = models.ManyToManyField("users.User", related_name="retested_posts", blank=True)
+    replies = models.ManyToManyField("Post", related_name="replied_to", blank=True)
+    mentions = models.ManyToManyField("users.User", related_name="mentioned_in_post", blank=True)
+    hashtags = models.ManyToManyField("PostHashtag", related_name="post_with_hashtag", blank=True)
+    is_reposted = models.BooleanField(default=False)
+    reposted_status = models.ForeignKey("Post", on_delete=models.CASCADE, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
     views = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     visibility = models.CharField(max_length=10, choices=Visibility.choices, default=Visibility.EVERYONE)
@@ -20,3 +27,10 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.content)[:5]
+
+
+class PostHashtag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
