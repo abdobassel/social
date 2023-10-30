1cf3c0b5238477ed2941afb0b4f37d99cbea99f4
hudaifa <hudaifa890@gmail.com>
2023-10-30 11:14:52 +0300

    slug hashtag generated

--- social/posts/signals.py
+++ social/posts/signals.py
@@ -2,10 +2,16 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.utils.text import slugify
 
-from social.posts.models import Post
+from social.posts.models import Post, PostHashtag
 
 
 @receiver(pre_save, sender=Post)
 def generate_slug(sender, instance, **kwargs):
     if not instance.slug:
         instance.slug = slugify(instance.content)
+
+
+@receiver(pre_save, sender=PostHashtag)
+def generate_hashtag_slug(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.name)
