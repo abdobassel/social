be032abdd491688faa0e47e3515dd9a4b622daf8
hudaifa <hudaifa890@gmail.com>
2023-10-30 11:15:02 +0300

    APIView

--- social/posts/views.py
+++ social/posts/views.py
@@ -1,23 +1,53 @@
-from rest_framework.generics import ListAPIView, ListCreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Post, PostHashtag
 from .serializers import PostHashtagSerializer, PostSerializer
 from ..users.models import User
 
 
-class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all().order_by("-timestamp")
+class PostListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
+    @staticmethod
+    def get(request, *args, **kwargs):
+        queryset = Post.objects.all().order_by("-timestamp")
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
 
-class PostListCreateView(ListCreateAPIView):
-    queryset = Post.objects.order_by("-timestamp")
-    serializer_class = PostSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=User.objects.get(pk=1))
+class PostListCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def get(request, format=None):
+        # Get the queryset of posts and serialize them
+        queryset = Post.objects.order_by("-timestamp")
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @staticmethod
+    def post(request, format=None):
+        # Create a new post using the request data
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=User.objects.get(pk=1))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostHashtagListAPIView(ListAPIView):
-    queryset = PostHashtag.objects.all()
+    # queryset = PostHashtag.objects.all()
+    # serializer_class = PostHashtagSerializer
+
     serializer_class = PostHashtagSerializer
+
+    @staticmethod
+    def get(request, *args, **kwargs):
+        queryset = PostHashtag.objects.all().order_by("-hashtag_time")
+        serializer = PostHashtagSerializer(queryset, many=True)
+        return Response(serializer.data)
