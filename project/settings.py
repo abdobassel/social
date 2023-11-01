ab4ce4d208e654a8cd313d2c0c514272a4f9e016
hudaifa <hudaifa890@gmail.com>
2023-11-01 19:31:14 +0300

    db

--- project/settings.py
+++ project/settings.py
@@ -89,14 +89,20 @@ if DEBUG:
     ]
 WSGI_APPLICATION = "project.wsgi.application"
 
-# Database https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# Database https://docs.djangoproject.com/en/4.2/ref/settings/#databases django.db.backends.postgresql
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
     }
 }
-
 # Password validation https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
