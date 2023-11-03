ce3d7461aa731c05fccd55d40ead00dd0ec68158
hudaifa <hudaifa890@gmail.com>
2023-11-03 17:26:57 +0300

    photo api

--- social/utils/profile_photo_api.py
+++ social/utils/profile_photo_api.py
@@ -13,17 +13,3 @@ def profile_photo_path(instance, filename, **kwargs):
     name, ext = get_profile_filename_extension(filename)
     final_filename = f"{instance.user.username}_{ext}"
     return f"profile/{new_filename}/{final_filename}"
-
-
-def get_post_filename_extension(filepath):
-    base_name = os.path.basename(filepath)
-    name, ext = os.path.splitext(base_name)
-    return name, ext
-
-
-def post_photo_path(instance, filename, **kwargs):
-    new_filename = instance.id
-    # final_filename = instance.user.username
-    name, ext = get_post_filename_extension(filename)
-    final_filename = f"{instance.user.username}_{ext}"
-    return f"post/{new_filename}/{final_filename}"
