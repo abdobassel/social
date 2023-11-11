edeb036d874ac7f794130fb0cc5a2ec4377d6e35
hudaifa <hudaifa890@gmail.com>
2023-11-11 21:34:21 +0300

    follows

--- social/profiles/api/urls.py
+++ social/profiles/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .follows import FollowersAPIView, FollowingAPIView, UnfollowAPIView, FollowAPIView
 from .views import (
     ProfileListAPIView,
     ProfileRetrieveAPIView,
@@ -13,4 +14,10 @@ urlpatterns = [
     path("me/", ProfileRetrieveAPIView.as_view(), name="my-profile"),
     path("update/", ProfileUpdateAPIView.as_view(), name="update-profile"),
     path("photo/<int:pk>/", ProfilePhotoAPIView.as_view(), name="photo-profile-api"),
+    # Following
+    path("me/followers/", FollowersAPIView.as_view(), name="followers"),
+    path("<int:pk>/follow/", FollowingAPIView.as_view(), name="follow"),
+    path("<int:pk>/unfollow/", UnfollowAPIView.as_view(), name="unfollow"),
+    path("<int:pk>/-F-follow/", FollowAPIView.as_view(), name="follow-f"),
+
 ]
