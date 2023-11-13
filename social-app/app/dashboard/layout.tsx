bfca35fc88e895e8362f14db915129ad4d918c73
hudaifa <hudaifa890@gmail.com>
2023-11-14 02:31:45 +0300

    import react

--- social-app/app/dashboard/layout.tsx
+++ social-app/app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
-import { RequireAuth } from '@/components/utils';
+import {RequireAuth} from '@/components/utils';
+import React from "react";
 
 interface Props {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-export default function Layout({ children }: Props) {
-	return <RequireAuth>{children}</RequireAuth>;
+export default function Layout({children}: Props) {
+    return <RequireAuth>{children}</RequireAuth>;
 }
