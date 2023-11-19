7223602625e48ef29a7e23163e00224594b08a79
hudaifa <hudaifa890@gmail.com>
2023-11-20 00:18:21 +0300

    serializers test

--- social/tests/posts/test_serializers.py
+++ social/tests/posts/test_serializers.py
@@ -1,13 +1,10 @@
 import pytest
 from django.contrib.auth import get_user_model
 from model_bakery import baker
-from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory
 
-from social.posts.api.serializers import UserSerializer, PostHashtagSerializer, PostFileMediaSerializer
+from social.posts.api.serializers import UserSerializer, PostHashtagSerializer, PostFileMediaSerializer, PostSerializer
 from social.posts.models import Post, PostHashtag
-
-
-# from social.posts.serializers import PostSerializer, PostHashtagSerializer, UserSerializer, PostFileMediaSerializer
 
 
 @pytest.fixture
@@ -25,6 +22,7 @@ def hashtag():
     return baker.make(PostHashtag, name="test-hashtag", slug="test-hashtag", is_active=True)
 
 
+@pytest.mark.django_db
 def test_user_serializer():
     user = baker.make(get_user_model())
     serializer = UserSerializer(user)
@@ -32,6 +30,7 @@ def test_user_serializer():
     assert "username" in serializer.data
 
 
+@pytest.mark.django_db
 def test_post_hashtag_serializer():
     hashtag = baker.make(PostHashtag, name="test-hashtag", slug="test-hashtag", is_active=True)
     serializer = PostHashtagSerializer(hashtag)
@@ -41,6 +40,7 @@ def test_post_hashtag_serializer():
     assert "is_active" in serializer.data
 
 
+@pytest.mark.django_db
 def test_post_file_media_serializer(post):
     serializer = PostFileMediaSerializer(post)
     assert "id" in serializer.data
@@ -51,8 +51,14 @@ def test_post_file_media_serializer(post):
     assert "views" in serializer.data
 
 
-def test_post_serializer(user, hashtag):
-    request = Request()
+@pytest.fixture
+def api_factory():
+    return APIRequestFactory()
+
+
+@pytest.mark.django_db
+def test_post_serializer(user, hashtag, api_factory):
+    request = api_factory.post("/api/posts/", {"format": "json"})
     data = {
         "user": user.id,
         "content": "Test content",
@@ -66,4 +72,4 @@ def test_post_serializer(user, hashtag):
     assert post.content == "Test content"
     assert post.hashtags.count() == 1
     assert post.hashtags.first() == hashtag
-    assert post.likes_count == 0
+    assert serializer.data["likes_count"] == 0
